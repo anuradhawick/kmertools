52094530868c92d87ab73570c992125bc67d4bc8
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2024-05-13 18:08:10 +0930

    TST: correct test by stripping special chars at ends

--- ktio/src/fops.rs
+++ ktio/src/fops.rs
@@ -11,7 +11,11 @@ pub fn delete_file_if_exists<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
 pub fn load_lines_sorted(path: &str) -> Vec<String> {
     let data = fs::read(path).unwrap();
     let text = String::from_utf8(data).unwrap().trim().to_string();
-    let mut arr: Vec<String> = text.split('\n').map(|s| s.to_string()).collect();
+    let mut arr: Vec<String> = text
+        .trim()
+        .split('\n')
+        .map(|s| s.trim().to_string())
+        .collect();
     arr.sort();
     arr
 }
